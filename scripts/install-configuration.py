fe98ca77690e4701647ee78cc9740df357779afd
Paul Ngouembe <paul.ngouembe@gmail.com>
2023-11-09 15:12:15 +0100

    fix: change the rootdir for install-configuration.py

--- scripts/install-configuration.py
+++ scripts/install-configuration.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
-from rich import print
 from pathlib import Path
 
+from rich import print
+
+
 def main():
-    dotfile_root = Path(__file__).parent.parent
+    dotfile_root = Path().home() / "dotfiles"
+
+    print(f"Searching for link file from {dotfile_root}")
 
     links_files = dotfile_root.rglob("links.prop")
     for links_file in links_files:
@@ -12,16 +16,17 @@ def main():
             source, target = link.split("=")
             source = source.replace("$DOTFILES", str(dotfile_root))
             target = target.replace("$HOME", str(Path().home()))
-            
+
             target = Path(target)
             target.parent.mkdir(parents=True, exist_ok=True)
-            
+
             target.unlink(missing_ok=True)
             target.symlink_to(source)
-        
+            print(f"Link created: {source} -> {target}")
+
     env_setup_script = Path().home() / ".env.sh"
     env_setup_script.write_text(f"export DOTFILES={dotfile_root}")
 
+
 if __name__ == "__main__":
     main()
-
