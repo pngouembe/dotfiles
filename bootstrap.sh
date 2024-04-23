23ebfe1402232455314419bc2da698232b3e35d7
Paul Ngouembe <paul.ngouembe@gmail.com>
2024-04-23 20:18:06 +0200

    feat: add the bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+sudo apt update && sudo apt install -y curl build-essential
+
+# Install cargo
+echo "Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+echo "Installing cargo binstall"
+$HOME/.cargo/bin/cargo install cargo-binstall
+
+echo "Installing just"
+$HOME/.cargo/bin/cargo binstall -y just
+
+exec $SHELL
