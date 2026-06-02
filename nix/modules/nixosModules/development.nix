f1262e7b96ca6eae1e467dedb78ad6326f3e7bb9
Paul Ngouembe <paul.ngouembe@gmail.com>
2026-06-02 20:05:34 +0200

    update development config

--- nix/modules/nixosModules/development.nix
+++ nix/modules/nixosModules/development.nix
@@ -1,21 +1,25 @@
 { ... }:
 {
-  flake.nixosModules.development = { pkgs, ... }: {
-    programs.nix-ld.enable = true;
+  flake.nixosModules.development =
+    { pkgs, ... }:
+    {
+      programs.nix-ld.enable = true;
 
-    virtualisation.docker.enable = true;
-    users.users.png.extraGroups = [ "docker" ];
+      virtualisation.docker.enable = true;
+      users.users.png.extraGroups = [ "docker" ];
 
-    environment.systemPackages = with pkgs; [
-      git
-      nixfmt
-      nil
-      nixd
-      zed-editor
-      gcc
-      nodejs
-      claude-code
-      lmstudio
-    ];
-  };
+      environment.systemPackages = with pkgs; [
+        git
+        nixfmt
+        nil
+        nixd
+        zed-editor
+        gcc
+        nodejs
+        claude-code
+        lmstudio
+        android-studio
+        uv
+      ];
+    };
 }
