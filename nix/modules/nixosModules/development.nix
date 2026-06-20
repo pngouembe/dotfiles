49e8c9ea18d8f3b2090620699a804cb2f7ff79f6
Paul Ngouembe <paul.ngouembe@gmail.com>
2026-06-21 01:07:23 +0200

    nix: allow docker containers to reach host

--- nix/modules/nixosModules/development.nix
+++ nix/modules/nixosModules/development.nix
@@ -6,6 +6,9 @@
       programs.nix-ld.enable = true;
 
       virtualisation.docker.enable = true;
+      # Make the `host.docker.internal:host-gateway` mapping resolve to the
+      # docker0 bridge gateway so containers can reach services on the host.
+      virtualisation.docker.daemon.settings.host-gateway-ip = "172.17.0.1";
       users.users.png.extraGroups = [ "docker" ];
 
       environment.systemPackages = with pkgs; [
